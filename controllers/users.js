c9c1f3c7d282326234d0588c847be34945ae1c15
aalbina <aalmukhametova@gmail.com>
2018-04-07 15:09:36 +0300

    fixes

--- controllers/users.js
+++ controllers/users.js
@@ -3,24 +3,15 @@ const UsersModel = require('../models/user');
 exports.create = (req, res) => {
     user = new UsersModel({
         username: req.body.username,
-<<<<<<< HEAD
         password: req.body.password,
-=======
-        password: req.body.password
->>>>>>> bb68a527ae3a72e36f9a62b48453a325f9c0a5af
     });
 
     user.save(function (err) {
         if (!err) {
             return res.status(200).send(user);
-<<<<<<< HEAD
         } else {
             return res.status(500).send(err);
         }
-=======
-        }
-        return res.status(500).send(err);
->>>>>>> bb68a527ae3a72e36f9a62b48453a325f9c0a5af
     });
 };
 
@@ -28,17 +19,10 @@ exports.item = (req, res) => {
     UsersModel.findById(req.params.id, function (err, user_data) {
         if (!err) {
             return res.status(200).send(user_data);
-<<<<<<< HEAD
         } else {
             console.log(err);
             return res.status(500).send(err);
         }
-    });
-};
-=======
-        }
-        console.log(err);
-        return res.status(500).send(err);
     });
 };
 
@@ -47,4 +31,3 @@ exports.doLogin = function (req, res) {
         res.redirect('/');
     });
 };
->>>>>>> bb68a527ae3a72e36f9a62b48453a325f9c0a5af
